14aeac9b691e42ae6ed4e14e202c72a9058df618
diegoramires05 <diego.ramires@syngenta.com>
2023-08-12 15:52:07 -0400

    feat: Aula 3

---
+++ aula_03.js
@@ -0,0 +1,12 @@
+const idade = 18;
+const nome = 'julio'
+let podeEntrar = false;
+
+// || = or && = and
+// == é um comparativo
+// === é 
+if (idade === '18'){
+    console.log("Entrou")
+} else {
+    console.log("Não entra")
+}
