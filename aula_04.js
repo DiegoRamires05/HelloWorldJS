9b5fbad6a5cf45941baedf6a2e2fc29bea458b48
diegoramires05 <diego.ramires@syngenta.com>
2023-08-12 16:02:40 -0400

    feat: Aula 4

---
+++ aula_04.js
@@ -0,0 +1,9 @@
+const quantidade = 10;
+let soma = 0;
+for(let a = 0; a<= quantidade;a++){
+    console.log("a=",a);
+    soma = soma+a;
+    console.log("soma=",soma);
+}
+
+console.log("Acabou ", soma);
